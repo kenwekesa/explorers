d12ea13998e1becaeb702748ba1bd477c50dcbdc
kenwekesa <kenwek1994@gmail.com>
2023-09-11 14:20:00 -0700

    changes to routing

--- virtualassistant/frontend/src/App.js
+++ virtualassistant/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 
 
---
+++ virtualassistant/frontend/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Home(props) {
+    return (
+        <div className='main'>
+            <Navbar className='navbar'/>
+            <Footer className='footer'/>
+        </div>
+    );
+}
+
+export default Home;
