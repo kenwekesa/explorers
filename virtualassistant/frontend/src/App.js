c22f660e2d289c51bd0f274d570d0a87a3c8d193
kenwekesa <kenwek1994@gmail.com>
2023-09-11 14:08:08 -0700

    changes files

---
+++ virtualassistant/frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <Routes>
+            
+    <Route path="/" element={<Home/>} />
+    <Route path="/services" element={<Home/>} />
+    <Route path="/prices" element={<Home/>}/ >
+    
+  </Routes>
+  );
+}
+
+export default App;
