4a2e2d4c3f7b7bcd847b59c4e2a6428ef14c56b8
kenwekesa <kenwek1994@gmail.com>
2023-09-20 23:11:02 +0300

    created table for order history

--- virtualassistant/frontend/src/pages/orderhistory/Orderhistory.jsx
+++ virtualassistant/frontend/src/pages/orderhistory/Orderhistory.jsx
@@ -1,19 +1,128 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './orderhistory.scss'
 import Navbar from '../../components/client_dashboard/navbar/Navbar'
 import Footer from '../../components/client_dashboard/footer/Footer'
 
 function Orderhistory() {
+  const data = [
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Kirisi',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Merisi',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Thomlo',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+    { id: 1, service: 'Item 1',plan:"Starter",assistants:2, period: '1 year', time_zone: 'EAT', amount:'10000', date:'12/3/23',status:'Completed'},
+
+    // ... more data
+  ];
+
+  
+  const [perPage, setPerPage] = useState(10); // Number of items per page
+  const [currentPage, setCurrentPage] = useState(1); // Current page
+  const [searchText, setSearchText] = useState(''); // Search input
+  const filteredData = data.filter((item) =>
+    item.service.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  // Calculate pagination values
+  const totalItems = filteredData.length;
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  // Function to paginate the data
+  const paginatedData = filteredData.slice(
+    (currentPage - 1) * perPage,
+    currentPage * perPage
+  );
   return (
     <div className='orderhistory'>
        <Navbar className='orderhistory_navbar'/>
        <div className="orderhistory-content">
-        {/* Your page content goes here */}
+        {/* Filter and Search */}
+        <div className="filter-search">
+          <div className="filter">
+            <label>Show:</label>
+            <select onChange={(e) => setPerPage(Number(e.target.value))}>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+            entries
+          </div>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </div>
+        </div>
+
+        {/* Table */}
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Service</th>
+              <th>Plan</th>
+              <th>Assistants</th>
+              <th>Period</th>
+              <th>Time zone</th>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedData.map((item,i) => (
+              <tr key={item.id}>
+                <td>{i+1}</td>
+                <td>{item.service}</td>
+                <td>{item.plan}</td>
+                <td>{item.assistants}</td>
+                <td>{item.period}</td>
+                <td>{item.time_zone}</td>
+                <td>{item.amount}</td>
+                <td>{item.date}</td>
+                <td>{item.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Pagination */}
+        <div className="pagination">
+          <div className="pagination-info">
+            Showing {perPage * (currentPage - 1) + 1} -{' '}
+            {perPage * currentPage} of {totalItems} entries
+          </div>
+          <div className="pagination-buttons">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage(currentPage - 1)}
+            >
+              Previous
+            </button>
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              Next
+            </button>
+          </div>
+        </div>
       </div>
-       <Footer className="orderhistory_footer"/>
+      <Footer className="orderhistory_footer" />
     </div>
-  )
+  );
 }
 
-export default Orderhistory
+export default Orderhistory;
