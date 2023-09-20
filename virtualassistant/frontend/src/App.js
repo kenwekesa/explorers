ccf8011716686b9b9bf190654cd46919d984bc66
Okellothomas <thomasolal33@gmail.com>
2023-09-20 21:36:08 +0300

    add changes

--- virtualassistant/frontend/src/App.js
+++ virtualassistant/frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import MainAbout from './FComponents/about/MainAbout';
 import './App.css';
-<<<<<<< HEAD
 import MainContact from './FComponents/contact/MainContact';
 import MainIndustries from './FComponents/industries/MainIndustries';
 import Mlanding from './FComponents/landing/Mlanding';
@@ -9,15 +8,7 @@ import Mprice from './FComponents/pricing/Mprice';
 import MainService from './FComponents/services/MainService';
 import Mainlogin from './FComponents/login/Mainlogin';
 import Mainsignup from './FComponents/signup/Mainsignup';
-
-=======
-import MainContact from './contact/MainContact';
-import MainIndustries from './industries/MainIndustries';
-import Mlanding from './landing/Mlanding';
-import Mprice from './pricing/Mprice';
-import MainService from './services/MainService';
 import Orderhistory from './pages/orderhistory/Orderhistory';
->>>>>>> ken
 
 
 function App() {
@@ -29,15 +20,11 @@ function App() {
         <Route path="/prices" element={<MainService /> } />
         <Route path="/services" element={<Mprice/> } />
         <Route path="/contact" element={<MainContact />} />
-<<<<<<< HEAD
         <Route path="/industries" element={<MainIndustries />} />
         <Route path='/login' element={<Mainlogin />} />
         <Route path='/signup' element={<Mainsignup />} />
-=======
         <Route path="/industries" element={ <MainIndustries /> } />
         <Route path="/orderhistory" element={ <Orderhistory/> } />
-
->>>>>>> ken
       </Routes>
     </div>
   );
