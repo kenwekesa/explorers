c4361f1462a1849be7e604ad63d84e8d7d8fe772
Okellothomas <thomasolal33@gmail.com>
2023-09-27 16:54:10 +0300

    add changes

--- virtualassistant/frontend/src/pages/servicelist/Mainservicelist.js
+++ virtualassistant/frontend/src/pages/servicelist/Mainservicelist.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../../components/client_dashboard/footer/Footer'
 import Navbar from '../../components/client_dashboard/navbar/Navbar'
 import "./Mainservicelist.css"
+import Plancard from './plancards/Plancard'
 
 const Mainservicelist = () => {
   return (
@@ -10,9 +11,18 @@ const Mainservicelist = () => {
         <Navbar />
       </div>
       <div className='addpagecontent'>
-        <p className='addnewplan'>New Plan</p>
+        <p className='addnewplan'>Available Plans</p>
       <div className='addplancards'>
-        </div>
+         <Plancard />         
+      </div>
+      <div className='addmainupdates'>
+      </div>
+      </div>
+      <div className='addpagecontent'>
+        <p className='addnewplan'>Available Services</p>
+      <div className='addplancards'>
+         <Plancard />         
+      </div>
       <div className='addmainupdates'>
       </div>
       </div>
