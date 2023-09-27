59334875e472ccae3d3a5c00389dd10718fc61e4
Okellothomas <thomasolal33@gmail.com>
2023-09-27 14:46:37 +0300

    add changes

--- virtualassistant/frontend/src/pages/addfund/addfund/Creditcard.js
+++ virtualassistant/frontend/src/pages/addfund/addfund/Creditcard.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import "./Creditcard.css"
+import visa from "../../../images/visa.png"
+import wise from "../../../images/wise.png"
+import paypal from "../../../images/paypal.png"
 
 const Creditcard = () => {
   return (
       <div className='creditcard'>
           <p className='paragraph-payment-options'>Available Payment Options</p>
           <div className='creditcard-options'>
-          <div className='credicard-active-options'>
+          <div className='credicard-active-option'>
               <p className='paragraph-option-titles'>Method</p>
               <p className='paragraph-option-titles'>Status</p>
           </div>
               <hr className='credit-card-hr'></hr>
             <div className='credicard-active-options'>
-              <p className='ton'>Method</p>
+                <a><img src={ visa} loading="eager" alt="visa" />Credit Card</a>
               <p className='ton tan'>Active</p>
           </div>
               <hr className='credit-card-hr'></hr>
            <div className='credicard-active-options'>
-              <p className='ton'>Method</p>
+              <a><img src={paypal} loading="eager" alt="paypal" /> Paypal</a>
               <p className='ton tan'>Active</p>
           </div>
               <hr className='credit-card-hr'></hr>
            <div className='credicard-active-options'>
-              <p className='ton'>Method</p>
+              <a><img src={ wise} loading="eager" alt="wise" /> Wise</a>
               <p className='ton tan'>Active</p>
           </div>
           </div>
