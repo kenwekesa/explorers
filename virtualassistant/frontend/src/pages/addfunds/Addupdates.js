ac2ab4afdf0eacbee93214e8f5d4e64c3f2689d7
Okellothomas <thomasolal33@gmail.com>
2023-09-27 15:07:48 +0300

    add changes

--- virtualassistant/frontend/src/pages/addfunds/Addupdates.js
+++ virtualassistant/frontend/src/pages/addfunds/Addupdates.js
@@ -38,26 +38,6 @@ const Addupdates = () => {
               onClose={closeDialog} 
               />
           </div>
-        {/* <div className='cardupdates'>   
-        <p className='paragraph-word-limit'>This is the first thing we should be doing in the first place,
-            this is the first thing we should be doing in the first place,
-            this is the first thing we should be doing in the first place</p>
-       <button className='ton tin tun' onClick={openDialog}>Read Update</button>
-        <Addadmincard
-              isOpen={isDialogOpen}
-              onClose={closeDialog} 
-              />
-          </div>
-        <div className='cardupdates'>   
-        <p className='paragraph-word-limit'>This is the first thing we should be doing in the first place,
-            this is the first thing we should be doing in the first place,
-            this is the first thing we should be doing in the first place</p>
-       <button className='ton tin tun' onClick={openDialog}>Read Update</button>
-        <Addadmincard
-              isOpen={isDialogOpen}
-              onClose={closeDialog} 
-              />
-      </div> */}
     </div>
   )
 }
