c211088822e4727ff9cc8be07c2daffba29461e2
Okellothomas <thomasolal33@gmail.com>
2023-09-27 13:33:45 +0300

    add content

---
+++ virtualassistant/frontend/src/pages/addfund/addfund/Cardbtn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Cardbtn.css"
+
+const Cardbtn = () => {
+  return (
+    <div className='cardbtn'>
+       <div><a className='ton tin'>STRIPE</a></div>  
+       <div><a className='ton tin'>PAYPAL</a></div> 
+       <div><a className='ton tin'>BANK/OTHER</a></div> 
+    </div>
+  )
+}
+
+export default Cardbtn
