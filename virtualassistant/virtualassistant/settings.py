df35174f9ce11b76c635730d41c07ebdb980114a
oyesa <mercyoyesa2018@gmail.com>
2023-10-20 12:58:41 +0300

    debug backend

--- virtualassistant/virtualassistant/settings.py
+++ virtualassistant/virtualassistant/settings.py
@@ -54,15 +54,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-
+    'django.middleware.common.CommonMiddleware',
 ]
 
 # Configure CORS settings
+
+# Access-Control-Allow-Origin: ["http://localhost:3000"]
+
 CORS_ORIGIN_ALLOW_ALL=True
 #CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Adjust this to your React app's URL
+    "http://localhost:3000",
 ]
+
 
 
 
